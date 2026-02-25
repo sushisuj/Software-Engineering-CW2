dfa9b07ebf330b00fe116003daad380755a9a5bb
BenjaminRAC <164635027+BenjaminRAC@users.noreply.github.com>
2026-02-25 13:06:11 +0000

    Add Recipe model with various attributes

--- backend/models.py
+++ backend/models.py
@@ -33,3 +33,18 @@ class Preference(Base):
     preference_id = Column(Integer, primary_key=True, index=True)
     preference_name = Column(String)
     like = Column(Integer)
+
+class Recipe(Base):
+    __tablename__ = "Recipe"
+    recipe_id = Column(Integer, primary_key=True, index=True)
+    complexity_id = Column(Integer, foreign_key("complexity_id"), primary_key=True)
+    recipe_name = Column(String)
+    cuisine = Column(String)
+    prep_time = Column(Integer)
+    cost_estimate = Column(Real)
+    calories = Column(Integer)
+    protien = Column(Integer)
+    carbs = Column(Integer)
+    fat = Column(Integer)
+    
+    
