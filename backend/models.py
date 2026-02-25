ae747a58424da53d6c9a07d914ff3ac7894a962c
James  Hind <jameshind2005@gmail.com>
2026-02-25 12:48:41 +0000

    Refactor models.py for improved readability and organization

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, Float
 from database import Base
 
 class User(Base):
@@ -8,3 +8,21 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
+
+class Ingredients(Base):
+    __tablename__ = "ingredients"
+
+    id = Column(Integer, primary_key=True, index=True)
+    ingredient_name = Column(String, index=True)
+    avg_calories = Column(Integer)
+    avg_protein = Column(Integer)
+    avg_carbs = Column(Integer)
+    avg_fat = Column(Integer)
+    avg_cost = Column(Float)
+
+class RecipeComplexity(Base):
+    __tablename__ = "recipe_complexity"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    time_passed = Column(Integer, index=True)
